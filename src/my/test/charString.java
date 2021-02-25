f0ceb8afaeae10c8e8670d169c5be60f35b5b865
pysolver33 <pysolver33@gmail.com>
2021-02-25 19:57:26 +0800

    tutorials

---
+++ src/my/test/charString.java
@@ -0,0 +1,14 @@
+package src.my.test;
+//H3110 w0r1d 2.0 true
+public class charString{
+    public static void main(String[] args) {
+        byte zero = 0;
+        short one = 1;
+        int number = 3;
+        char H = 'H',r = 'r',d = 'd';
+        float v2 = 2.0f;
+        boolean True = true;
+        String output = zero+" w"+zero+r+one+d+' '+v2+' '+True;
+        System.out.printf("%c%d%d%d%s\n",H,number,one,one,output);
+    }
+}
---
+++ src/my/test/hellodidnotwork.java
@@ -0,0 +1,8 @@
+package src.my.test;
+
+public class hellodidnotwork {
+    public static void main(String[] args) {
+        System.out.println("This will be printed");
+    }
+}
+
