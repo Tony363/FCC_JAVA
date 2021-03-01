d04267fd773a5aca7a5c6f8d1dca93854e03d3d3
pysolver33 <pysolver33@gmail.com>
2021-03-01 10:29:58 +0800

    tutorials

---
+++ src/sand/box/Bool.java
@@ -0,0 +1,18 @@
+package src.my.test;
+
+public class Bool {
+    public static void main(String[] args) {
+        String a = new String("Wow");
+        String b = "Wow";
+        String c = a;
+        String d = c;
+
+        boolean b1 = a == b;
+        boolean b2 = d.equals(b + "!");
+        boolean b3 = !c.equals(a);
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+    }
+}
--- src/sand/box/hellodidnotwork.java
+++ src/sand/box/hellodidnotwork.java
@@ -1,4 +1,4 @@
-package src.my.test;
+package src.sand.box;
 
 public class hellodidnotwork {
     public static void main(String[] args) {
