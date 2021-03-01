8b551630a945b1637dd6022dc9ed3c2e4d370e31
pysolver33 <pysolver33@gmail.com>
2021-03-01 12:04:35 +0800

    tutorials

---
+++ src/sand/box/command_args.java
@@ -0,0 +1,10 @@
+package src.sand.box;
+
+public class command_args {
+    public static void main(String[] args) {
+        // write your code here
+        for (String out :  args){
+            System.out.println(out);
+        }
+    }
+}
---
+++ src/sand/box/inheritance.java
@@ -0,0 +1,60 @@
+package src.sand.box;
+
+import java.util.Scanner;
+
+class Door {
+    public void open ()
+    {
+        push();
+    }
+    public void pull (){
+        System.out.println("pulled at door");
+    }
+    public void push(){
+        System.out.println("pushed at door");
+
+    }
+}
+
+class BankVaultDoor extends Door {
+    private String Combination;
+    public void open () {
+        enterCombination();
+        super.pull();
+    }
+    public void enterCombination(){
+        Scanner CombinationScanner = new Scanner(System.in);
+        System.out.println("Enter Combination:");
+        this.Combination = CombinationScanner.next();
+    }
+
+}
+
+class HouseFrontDoor extends Door {
+    // Your code goes here
+    public void open(){
+        this.UseKeys();
+        super.open();
+    }
+    public void UseKeys(){
+        System.out.println("kchink");
+    }
+}
+
+public class inheritance {
+    public static void main(String[] args){
+        Door door = new Door();
+        BankVaultDoor bankDoor = new BankVaultDoor();
+        HouseFrontDoor frontDoor = new HouseFrontDoor();
+        
+        System.out.println("testing doors:");
+        door.open();
+        System.out.println();
+        System.out.println("robbing the bank:");
+        bankDoor.open();
+        System.out.println();
+        System.out.println("stowing the money away:");
+        frontDoor.open();
+
+    }
+}
