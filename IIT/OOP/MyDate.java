47bae547e64271042b34c5ba5574b8645e232455
pysolver33 <pysolver33@gmail.com>
2021-04-13 10:01:09 +0800

    fix some function overloading

--- IIT/OOP/MyDate.java
+++ IIT/OOP/MyDate.java
@@ -1,59 +1,88 @@
 package IIT.OOP;
+import java.util.Arrays;
 
 class Date{
     private int year,month,day;
+    private int[] date;
     Date(){
         this.year = 1960;
         this.month = 1;
         this.day = 1;
+        this.date[0] = 1960;
+        this.date[1] = 1;
+        this.date[2] = 1;
     }
     Date(int year,int month,int day){
         this.setDate(year,month,day);
+    }
+    public int[] getDate(){
+        return Arrays.copyOf(this.date,this.date.length);
+    }
+    int[] getDate(){
+        return this.date;
+    }
+    void setDate(int[] date){
+        for (int i=0;i<this.date.length;i++){
+            this.date[i] = date[i];
+        }
+    }
+    public boolean setDate(int[] date){
+        if (isValidDate(date[0],date[1],date[2])){
+            for (int i=0;i<this.date.length;){
+                this.date[i] = date[i];
+            }
+            return true;
+        }
+        return false;
+    }
+    public boolean setDate(int year, int month, int day){
+        return setDate(new int[]{year,month,day});
     }
     /**
     @return return Date objects year.
      */
     public int getYear(){
-        return this.year;
-    }/**
+        return this.date[0];
+    }
+    /**
     return Date objects month.
     @return  return Date objects month.
     */
     public int getMonth(){
-        return this.month;
+        return this.date[1];
     }
     /**
      * 
      * @return Date objects day.
      */
     public int getDay(){
-        return this.day;
+        return this.date[2];
     }
-    void setYear(int year){
-        this.year = year;
-    }
-    void setMonth(int month){
-            this.month = month;
-    }
-    void setDay(int day){
-        this.day = day;
-    }
-    /**
-     *  public method to set year, month, day for this date object
-     * @param year
-     * @param month
-     * @param day
-     * @return
-     */
-    public boolean setDate(int year, int month,int day){
-        if (isValidDate(year,month,day)){
-            this.setYear(year);
-            this.setMonth(month);
-            this.setDay(day);
-            return true;
-        }
-        return false;
-    }
+    // void setYear(int year){
+    //     this.year = year;
+    // }
+    // void setMonth(int month){
+    //         this.month = month;
+    // }
+    // void setDay(int day){
+    //     this.day = day;
+    // }
+    // /**
+    //  *  public method to set year, month, day for this date object
+    //  * @param year
+    //  * @param month
+    //  * @param day
+    //  * @return
+    //  */
+    // public boolean setDate(int year, int month,int day){
+    //     if (isValidDate(year,month,day)){
+    //         this.setYear(year);
+    //         this.setMonth(month);
+    //         this.setDay(day);
+    //         return true;
+    //     }
+    //     return false;
+    // }
     /**
      * Checks whether the given year is aleap year.
      * The implementation is based on the 4-100-400 rule.
